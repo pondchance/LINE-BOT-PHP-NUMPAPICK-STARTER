c8894530093c4f5350fee79e651d6996e4843797
pondchance <31153850+pondchance@users.noreply.github.com>
2019-03-07 17:11:42 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'GnW1kmsBsYdaqox/iKYdrVXXLs9zGNmsUbJitzc0ruqavQL11OleTuem4jj4ijGIgHB4Q/l0uXZmznTlHXhJYAYany3JsunN6VlvJniaPHCpO1Mdj4dTfY8S1saYOAo47BF4XQHnMPUw0zjeOoskFgdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U99a1ae7b36379e2158ed6c95082784a9',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
